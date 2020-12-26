d1c1841f3944edc849c21b03370ef2ca4e25d1cc
Ranjith YK <Ranjith.Yk@Westpharma.com>
2020-12-26 13:58:14 +0530

    Implemented light weight version Tiny Ioc container only for xamarin

---
+++ XamarinIoc/IRegisterOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace XamarinIoc
+{
+    public interface IRegisterOptions
+    {
+        IRegisterOptions AsSingleton();
+        IRegisterOptions AsMultiInstance();
+        IRegisterOptions WithWeakReference();
+        IRegisterOptions WithStrongReference();
+        IRegisterOptions UsingConstructor<RegisterType>(Expression<Func<RegisterType>> constructor);
+    }
+}
